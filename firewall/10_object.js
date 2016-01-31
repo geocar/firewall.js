f2e1a827592f38f15bbb73ff2ec7dd310bc54485
Geo Carncross <geocar@gmail.com>
2016-01-31 08:17:55 +0000

    more

--- firewall/10_object.js
+++ firewall/10_object.js
@@ -1,5 +1,4 @@
 function intercept_object(obj) {
-console.log('obj',obj);
   element_attribute(obj, "data",     function() { return "" }, blocked);
   element_attribute(obj, "codeBase", function() { return "" }, blocked);
   element_attribute(obj, "archive",  function() { return "" }, blocked);
