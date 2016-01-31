47e8b93b34cc5c07814a5bbf794ed639bff598cc
Geo Carncross <geocar@gmail.com>
2016-01-31 09:23:36 +0000

    sneaky

---
+++ tests/a2.js
@@ -0,0 +1,9 @@
+var a=document.createElement("div");
+a.innerHTML='<a href="javascript:parent.postMessage(\'fail\',\'*\')">W</a>';
+document.body.appendChild(a);
+
+var x=3,y=3;
+var e=document.createEvent("MouseEvents");
+e.initMouseEvent("click", 
+  false,false,document.defaultView,0,x,y,x,y,false,false,false,false,0,a);
+a.firstChild.dispatchEvent(e);
