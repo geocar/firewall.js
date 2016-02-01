5a4599d27b5d7cfb6bb41de7f41effab1fdbabc6
Geo Carncross <geocar@gmail.com>
2016-02-01 12:12:57 +0000

    faster

--- tests/a3.js
+++ tests/a3.js
@@ -5,6 +5,7 @@ document.body.appendChild(a);
 var x=3,y=3;
 var e=document.createEvent("MouseEvents");
 e.initMouseEvent("click", 
-  false,false,document.defaultView,0,x,y,x,y,false,false,false,false,0,a);
+  false,true,document.defaultView,0,x,y,x,y,false,false,false,false,0,a);
 a.firstChild.dispatchEvent(e);
+
 setTimeout(function(){ parent.postMessage('block','*') },2);
