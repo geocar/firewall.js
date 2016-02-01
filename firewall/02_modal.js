09e274fff5bbe0efc10e0e7b7a290ac9e3eca496
Geo Carncross <geocar@gmail.com>
2016-02-01 12:13:42 +0000

    note

--- firewall/02_modal.js
+++ firewall/02_modal.js
@@ -1,3 +1,6 @@
+/* the <iframe> has sandbox enabled, so these should generate an exception
+   if these routines are called.
+*/
 window["alert"]  =function_named("alert",  blocked);
 window["prompt"] =function_named("prompt", blocked);
 window["confirm"]=function_named("confirm",blocked);
