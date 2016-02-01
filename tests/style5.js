6b59086ed504f046ad9a97b0f16176df7c7d70b4
Geo Carncross <geocar@gmail.com>
2016-02-01 17:55:58 +0000

    more

---
+++ tests/style5.js
@@ -0,0 +1,7 @@
+h=document.getElementsByTagName('head')[0];
+if(!h)document.documentElement.appendChild(h=document.createElement('head'));
+h.appendChild(s=document.createElement('style'))
+s.type='text/css';
+s=document.styleSheets[0];
+s.insertRule('body{color:red;}',0);
+s.cssRules[0].style.backgroundImage='url(http://www.example.com)';
