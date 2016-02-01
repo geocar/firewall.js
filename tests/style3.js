600c74a6b6c530a25f60f907070f3af4d6e5d24a
Geo Carncross <geocar@gmail.com>
2016-02-01 15:38:15 +0000

    more vectors

--- tests/style3.js
+++ tests/style3.js
@@ -2,4 +2,6 @@ h=document.getElementsByTagName('head')[0];
 if(!h)document.documentElement.appendChild(h=document.createElement('head'));
 h.appendChild(s=document.createElement('style'))
 s.type='text/css';
-s.innerHTML='body{background:url(http://www.example.com/);}';
+s=document.styleSheets[0];
+s.insertRule('body{background:url(http://www.example.com/);}',0);
+
