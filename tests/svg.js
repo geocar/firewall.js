d0f56c0d1c81214ed7878cb372485a4f702b85bc
Geo Carncross <geocar@gmail.com>
2016-02-01 16:26:32 +0000

    svg

---
+++ tests/svg.js
@@ -0,0 +1,3 @@
+document.body.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><image x="20" y="20" width="300" height="80" xlink:href="http://www.example.com/" /></svg>';
+
+
