2ffb36f16d0b675535edd6f630d7b50c5546e71e
Geo Carncross <geocar@gmail.com>
2016-02-01 15:36:41 +0000

    another way

---
+++ tests/style2.js
@@ -0,0 +1,5 @@
+h=document.getElementsByTagName('head')[0];
+if(!h)document.documentElement.appendChild(h=document.createElement('head'));
+h.appendChild(s=document.createElement('style'))
+s.type='text/css';
+s.innerHTML='body{background:url(http://www.example.com/);';
