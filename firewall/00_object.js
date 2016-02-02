7ee5d45b61f75ffce2315c6d88b055e5f89843ef
Geo Carncross <geocar@gmail.com>
2016-02-02 08:18:10 +0000

    oops

--- firewall/00_object.js
+++ firewall/00_object.js
@@ -1,6 +1,6 @@
 /* If new() were a function, this does what new.apply should do.  */
 function new_apply(klass, args) {
   args = [].slice.call(args);
-  var body = "return new klass(" + args.map(function(_,i){ return "args[i]" }).join(",") + ")";
+  var body = "return new klass(" + args.map(function(_,i){ return "args["+i+"]" }).join(",") + ")";
   return new Function("klass", "args", body)(klass, args);
 }
